26aacc374a371f3d202109c8821469f7320d7709
Dooblidoo <seb@lidstrom.fr>
2022-01-29 14:55:49 +0100

    Functions and Documentation

---
+++ tictactoe.py
@@ -0,0 +1,76 @@
+# A single player tictactoe game player, the bot make random moves
+
+
+def draw_board(board):
+    """
+        Input: list of ints
+        Output:
+        Renders a window and siplays the board.
+    """
+
+    return 0
+
+
+def player_move(board):
+    """
+        Input: list of ints
+        Output:
+        Asks for the players move and places it on the board.
+    """
+
+    return 0
+
+
+def switch_player(is_players_move):
+    """
+        Input: boolean
+        Output: boolean
+        Switches boolean value.
+    """
+
+    return is_players_move
+
+
+def full_board(board):
+    """
+        Input: list of ints
+        Output: boolean
+        If False the game continues, if True, the board is full. It is a tie.
+    """
+
+    return is_full
+
+
+def three_in_a_row(board):
+    """
+        Input: list of ints
+        Output: (boolean, int)
+        Checks if there are 3 symbols in a row and returns either:
+            True, int representing row, column or diagonal
+            False, None
+    """
+
+    return are_three_in_a_row, case
+
+
+def display_winner(player_symbol, case):
+    """
+        Input: string, int
+        Output:
+        Prints a string to stdout announcing the outcome of the game.
+    """
+
+    return 0
+
+
+def main():
+    """
+        Initialises and determines the flow the game
+    """
+
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
